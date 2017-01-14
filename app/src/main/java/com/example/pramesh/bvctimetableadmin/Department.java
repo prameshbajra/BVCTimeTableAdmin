eb9107167cf7eb1e21dd1800c79270541744eb5f
Suzal3579 <pe.messh@gmail.com>
2017-01-14 18:57:15 +0530

    Alert Dialog popping up once after app install ... working fine up untill now

--- app/src/main/java/com/example/pramesh/bvctimetableadmin/Department.java
+++ app/src/main/java/com/example/pramesh/bvctimetableadmin/Department.java
@@ -1,10 +1,10 @@
 package com.example.pramesh.bvctimetableadmin;
 
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +16,8 @@ public class Department extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_department);
+
+        checkFirstRun();
 
         civilButton = (Button) findViewById(R.id.civilButton);
         cseButton = (Button) findViewById(R.id.cseButton);
@@ -68,6 +70,25 @@ public class Department extends AppCompatActivity {
             }
         });
     }
+
+    public void checkFirstRun() {
+        boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isFirstRun", true);
+        if (isFirstRun) {
+            String title = "Welcome to BVC Time Table.\n";
+            String message = "This app is a server app and should not be installed by a student.\n\n\n-By Pramesh Bajracharya";
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setCancelable(true);
+            builder.setTitle(title);
+            builder.setMessage(message);
+            builder.show();
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                    .edit()
+                    .putBoolean("isFirstRun", false)
+                    .apply();
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
--- app/src/main/java/com/example/pramesh/bvctimetableadmin/TimeTablePage.java
+++ app/src/main/java/com/example/pramesh/bvctimetableadmin/TimeTablePage.java
@@ -3,7 +3,6 @@ package com.example.pramesh.bvctimetableadmin;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
